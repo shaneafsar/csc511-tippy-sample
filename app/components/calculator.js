7d4eb6b2bb56288c6f4b7c112532df89c87b5d56
Shane Afsar <safsar@linkedin.com>
2019-09-21 16:59:50 -0400

    Remove unnecessary property in controller and add extra spacing for readability in component.

--- app/components/calculator.js
+++ app/components/calculator.js
@@ -3,18 +3,23 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   totalBill: 0,
+
   tipValue: 0,
+
   tipAmount: computed('totalBill', 'tipValue', function() {
     return this.get('totalBill') * parseFloat(this.get('tipValue'));
   }),
+
   totalBillWithTip: computed('totalBill', 'tipAmount', function() {
     return this.get('tipAmount') + parseFloat(this.get('totalBill'));
   }),
+
   showTotal: computed('totalBillWithTip', function() {
     return (
       this.get('totalBillWithTip') > 0 && !isNaN(this.get('totalBillWithTip'))
     );
   }),
+
   didReceiveAttrs: function() {
     this._super(...arguments);
     const inputAmount = this.get('inputAmount');
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  queryParams: ['amount'],
-  test: 0
+  queryParams: ['amount']
 });
